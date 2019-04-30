8b61f915585d9a1528bfdd36fac514f41b33d4a8
Jason Estey <esteyj@yahoo.com>
2019-04-29 21:45:33 -0700

    move dice held state to dice object

--- dice.js
+++ dice.js
@@ -8,15 +8,32 @@ function createDice()
 		faces: die_faces,
 
 		last_roll: [],
+		is_held: {},
 
-		roll_d6: function (count)
+		roll: function (count, sides)
 		{
 			if (count === undefined) { count = 1; }
-			this.last_roll = [];
+			if (sides === undefined) { sides = 6; }
+
+			var result = [];
 			for (var i = 0; i < count; i++) {
-				this.last_roll.push(Math.floor(Math.random() * Math.floor(6)) + 1);
+				result.push(Math.floor(Math.random() * Math.floor(sides)) + 1);
 			}
+			return result;
+		},
+
+		roll_unheld: function (count, sides)
+		{
+			this.last_roll = this.roll(count, sides).map((value, index) => {
+				return this.is_held[index + 1] ? this.last_roll[index] : value;
+			});
+
 			return this.last_roll;
+		},
+
+		clear_held: function()
+		{
+			this.is_held = {};
 		},
 
 		to_string: function (roll_arr)
