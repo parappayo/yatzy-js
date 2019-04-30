3f930aedc3900f7ed227c42065130702aadae3fb
Jason Estey <esteyj@yahoo.com>
2019-04-29 21:32:50 -0700

    dice tracks last roll

--- dice.js
+++ dice.js
@@ -7,20 +7,23 @@ function createDice()
 	return {
 		faces: die_faces,
 
+		last_roll: [],
+
 		roll_d6: function (count)
 		{
 			if (count === undefined) { count = 1; }
-			var result = [];
+			this.last_roll = [];
 			for (var i = 0; i < count; i++) {
-				result.push(Math.floor(Math.random() * Math.floor(6)) + 1);
+				this.last_roll.push(Math.floor(Math.random() * Math.floor(6)) + 1);
 			}
-			return result;
+			return this.last_roll;
 		},
 
 		to_string: function (roll_arr)
 		{
+			roll_arr = Array.isArray(roll_arr) ? roll_arr : this.last_roll;
 			return roll_arr
-				.map((roll) => { return die_faces[roll]; })
+				.map((roll) => { return this.faces[roll]; })
 				.join(' ');
 		},
 	}
